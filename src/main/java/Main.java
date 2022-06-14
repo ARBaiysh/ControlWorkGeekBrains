b083819c90b08619842391d029a771ce38f129aa
ARBaiysh <o0052i@mail.ru>
2022-06-14 09:15:08 +0600

    add arrayOnlyUpToThreeCharacters method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,4 +5,17 @@ public class Main {
     private static String[] initArray() {
         return new String[]{"asd", "asdff", "as", "ass"};
     }
+
+    private static String[] arrayOnlyUpToThreeCharacters(String[] array) {
+        if (array.length == 0) {
+            return array;
+        }
+        StringBuilder st = new StringBuilder();
+        for (String s : array) {
+            if (s.length() < 4) {
+                st.append(s).append(",");
+            }
+        }
+        return st.toString().split(",");
+    }
 }
