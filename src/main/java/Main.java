ce6a4a01973f0164733106e8d3e13e50ab0db91d
ARBaiysh <o0052i@mail.ru>
2022-06-14 09:13:10 +0600

    add Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    private static String[] initArray() {
+        return new String[]{"asd", "asdff", "as", "ass"};
+    }
+}
